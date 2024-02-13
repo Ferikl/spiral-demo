652bea1528ff55b748e60402d8d0e6f395a66396
Pablo Bezdvernyi <pavlo.bezdvernyi@mojam.co>
2024-02-13 16:41:52 +0200

    Update readme and add cs fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'app/database/migrations',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@PER-CS' => true,
+        '@PER-CS2.0' => true,
+        '@PHP83Migration' => true,
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+    ;
